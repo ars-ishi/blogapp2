84e9ea78c6ab829ba37df633ab55e94f525d33f4
ars-ishi <ars.ishi11@gmail.com>
2018-11-14 22:24:20 +0900

    Create article_favorites table
    article_favoritesのテーブルを作成

---
+++ db/migrate/20181114111750_create_article_favorites.rb
@@ -0,0 +1,9 @@
+class CreateArticleFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :article_favorites do |t|
+      t.references :user, foreign_key: true
+      t.references :article, foreign_key: true
+      t.timestamps
+    end
+  end
+end
