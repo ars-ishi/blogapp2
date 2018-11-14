ed730272bdcdd080ad9b0f52910e77a05d533e0d
ars-ishi <ars.ishi11@gmail.com>
2018-11-14 22:34:09 +0900

    Edit models/user, models/article, models/article_favorite
    userモデル、articleモデル、article_favoriteモデルのアソシエーションを設定しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
+  has_many :article_favorites, dependent: :destroy
+  has_many :articles, through: :article_favorites
   enum role: { admin: 0, member: 1 }
   mount_uploader :ic_image, UserIcImageUploader
   mount_uploader :bg_image, UserBgImageUploader
